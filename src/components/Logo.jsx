bbe2242174f626b95b6c99e23be750915a484336
Shani Madar <shani@mly.co.il>
2026-05-09 23:29:51 +0300

    fix logo casing

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo({ variant = 'light', className = '' }) {
   return (
     <a href="/" className="block">
       <img
-        src="/logo-yaad.png"
+        src="./logo-yaad.png"
         alt="יעד שירותי ניקיון"
         className={className}
         style={{
